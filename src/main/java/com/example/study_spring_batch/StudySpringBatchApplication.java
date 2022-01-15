fd86dfba7c35113786793aa087abc1a41ea45a90
yunrap <dbsdpwl7@gmail.com>
2022-01-15 20:56:47 +0900

    Initial commit

---
+++ src/main/java/com/example/study_spring_batch/StudySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/study_spring_batch/StudySpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
