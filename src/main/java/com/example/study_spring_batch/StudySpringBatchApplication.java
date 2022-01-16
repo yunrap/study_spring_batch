74d6740a0d31ac924fbc15f52d659d9120523002
yunrap <dbsdpwl7@gmail.com>
2022-01-16 20:48:51 +0900

    batch 기본설정

--- src/main/java/com/example/study_spring_batch/StudySpringBatchApplication.java
+++ src/main/java/com/example/study_spring_batch/StudySpringBatchApplication.java
@@ -1,8 +1,10 @@
 package com.example.study_spring_batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing  // 스프링배치 작동 어노테이션
 @SpringBootApplication
 public class StudySpringBatchApplication {
 
