728dd97e6ab370b35fe0d373e32d86fe31f7c6b9
yunrap <dbsdpwl7@gmail.com>
2022-02-14 23:30:50 +0900

    수정

--- src/main/java/com/example/study_spring_batch/repository/TestResourceRepository.java
+++ src/main/java/com/example/study_spring_batch/repository/TestResourceRepository.java
@@ -1,6 +1,7 @@
 package com.example.study_spring_batch.repository;
 
 import com.example.study_spring_batch.domain.TestResource;
+import com.example.study_spring_batch.domain.TestResourceMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.expression.spel.ast.OpAnd;
 import org.springframework.stereotype.Repository;
@@ -8,9 +9,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TestResourceRepository extends JpaRepository<TestResource, Long> {
-       //int countByTcSeqAndTcDayAndVhclCode(Integer tcSeq, String planDay, String vhclCode);
+public interface TestResourceRepository extends JpaRepository<TestResourceMapping, Long> {
+       int countByTcSeqAndTcDayAndDriverNumber(Integer tcSeq, String planDay, int driverNumber);
 
-        Optional<TestResource> findByEmployeeNo(String employeeNo);
 
 }
